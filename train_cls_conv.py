77407fe34b9864b3600c7c923c270d3f10e38b26
longspringcity <1277535055@qq.com>
2021-01-12 18:49:26 +0800

    small changes

--- train_cls_conv.py
+++ train_cls_conv.py
@@ -1,35 +1,39 @@
 import argparse
-import os
-import torch
-import torch.nn.parallel
-import torch.utils.data
-import torch.nn.functional as F
-from data_utils.ModelNetDataLoader import ModelNetDataLoader
 import datetime
 import logging
+import os
 from pathlib import Path
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.utils.data
 from tqdm import tqdm
-from utils.utils import test, save_checkpoint
-from model.pointconv import PointConvDensityClsSsg as PointConvClsSsg
+
 import provider
-import numpy as np 
+from data_utils.ModelNetDataLoader import ModelNetDataLoader
+from model.pointconv import PointConvDensityClsSsg as PointConvClsSsg
+from utils.utils import test, save_checkpoint
 
 
 def parse_args():
     '''PARAMETERS'''
     parser = argparse.ArgumentParser('PointConv')
     parser.add_argument('--batchsize', type=int, default=32, help='batch size in training')
-    parser.add_argument('--epoch',  default=400, type=int, help='number of epoch in training')
+    parser.add_argument('--epoch', default=400, type=int, help='number of epoch in training')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--num_point', type=int, default=1024, help='Point Number [default: 1024]')
     parser.add_argument('--num_workers', type=int, default=16, help='Worker Number [default: 16]')
     parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer for training')
-    parser.add_argument('--pretrain', type=str, default=None,help='whether use pretrain model')
+    parser.add_argument('--pretrain', type=str, default=None, help='whether use pretrain model')
     parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate of learning rate')
     parser.add_argument('--model_name', default='pointconv', help='model name')
-    parser.add_argument('--normal', action='store_true', default=False, help='Whether to use normal information [default: False]')
+    parser.add_argument('--normal', action='store_true', default=False,
+                        help='Whether to use normal information [default: False]')
     return parser.parse_args()
+
 
 def main(args):
     '''HYPER PARAMETER'''
@@ -38,7 +42,8 @@ def main(args):
     '''CREATE DIR'''
     experiment_dir = Path('./experiment/')
     experiment_dir.mkdir(exist_ok=True)
-    file_dir = Path(str(experiment_dir) + '/%s_ModelNet40-'%args.model_name + str(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')))
+    file_dir = Path(str(experiment_dir) + '/%s_ModelNet40-' % args.model_name + str(
+        datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')))
     file_dir.mkdir(exist_ok=True)
     checkpoints_dir = file_dir.joinpath('checkpoints/')
     checkpoints_dir.mkdir(exist_ok=True)
@@ -50,11 +55,12 @@ def main(args):
     logger = logging.getLogger(args.model_name)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler(str(log_dir) + 'train_%s_cls.txt'%args.model_name)
+    file_handler = logging.FileHandler(str(log_dir) + 'train_%s_cls.txt' % args.model_name)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
-    logger.info('---------------------------------------------------TRANING---------------------------------------------------')
+    logger.info(
+        '---------------------------------------------------TRANING---------------------------------------------------')
     logger.info('PARAMETER ...')
     logger.info(args)
 
@@ -64,8 +70,10 @@ def main(args):
 
     TRAIN_DATASET = ModelNetDataLoader(root=DATA_PATH, npoint=args.num_point, split='train', normal_channel=args.normal)
     TEST_DATASET = ModelNetDataLoader(root=DATA_PATH, npoint=args.num_point, split='test', normal_channel=args.normal)
-    trainDataLoader = torch.utils.data.DataLoader(TRAIN_DATASET, batch_size=args.batchsize, shuffle=True, num_workers=args.num_workers)
-    testDataLoader = torch.utils.data.DataLoader(TEST_DATASET, batch_size=args.batchsize, shuffle=False, num_workers=args.num_workers)
+    trainDataLoader = torch.utils.data.DataLoader(TRAIN_DATASET, batch_size=args.batchsize, shuffle=True,
+                                                  num_workers=args.num_workers)
+    testDataLoader = torch.utils.data.DataLoader(TEST_DATASET, batch_size=args.batchsize, shuffle=False,
+                                                 num_workers=args.num_workers)
 
     logger.info("The number of training data is: %d", len(TRAIN_DATASET))
     logger.info("The number of test data is: %d", len(TEST_DATASET))
@@ -88,7 +96,6 @@ def main(args):
         print('No existing model, starting training from scratch...')
         start_epoch = 0
 
-
     if args.optimizer == 'SGD':
         optimizer = torch.optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
     elif args.optimizer == 'Adam':
@@ -107,16 +114,16 @@ def main(args):
 
     '''TRANING'''
     logger.info('Start training...')
-    for epoch in range(start_epoch,args.epoch):
+    for epoch in range(start_epoch, args.epoch):
         print('Epoch %d (%d/%s):' % (global_epoch + 1, epoch + 1, args.epoch))
-        logger.info('Epoch %d (%d/%s):' ,global_epoch + 1, epoch + 1, args.epoch)
+        logger.info('Epoch %d (%d/%s):', global_epoch + 1, epoch + 1, args.epoch)
         mean_correct = []
 
         scheduler.step()
         for batch_id, data in tqdm(enumerate(trainDataLoader, 0), total=len(trainDataLoader), smoothing=0.9):
             points, target = data
             points = points.data.numpy()
-            jittered_data = provider.random_scale_point_cloud(points[:,:, 0:3], scale_low=2.0/3, scale_high=3/2.0)
+            jittered_data = provider.random_scale_point_cloud(points[:, :, 0:3], scale_low=2.0 / 3, scale_high=3 / 2.0)
             jittered_data = provider.shift_point_cloud(jittered_data, shift_range=0.2)
             points[:, :, 0:3] = jittered_data
             points = provider.random_point_dropout_v2(points)
@@ -159,14 +166,14 @@ def main(args):
 
         print('\r Loss: %f' % loss.data)
         logger.info('Loss: %.2f', loss.data)
-        print('\r Test %s: %f   ***  %s: %f' % (blue('Accuracy'),acc, blue('Best Accuracy'),best_tst_accuracy))
+        print('\r Test %s: %f   ***  %s: %f' % (blue('Accuracy'), acc, blue('Best Accuracy'), best_tst_accuracy))
         logger.info('Test Accuracy: %f  *** Best Test Accuracy: %f', acc, best_tst_accuracy)
 
-
         global_epoch += 1
-    print('Best Accuracy: %f'%best_tst_accuracy)
+    print('Best Accuracy: %f' % best_tst_accuracy)
 
     logger.info('End of training...')
+
 
 if __name__ == '__main__':
     args = parse_args()
