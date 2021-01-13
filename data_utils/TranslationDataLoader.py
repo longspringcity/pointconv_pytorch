e28bc9cac07950282f78839236eae5c4f44b6821
longspringcity <1277535055@qq.com>
2021-01-13 13:43:25 +0800

    small changes

---
+++ data_utils/TranslationDataLoader.py
@@ -0,0 +1,96 @@
+import os
+import warnings
+
+import numpy as np
+from torch.utils.data import Dataset
+
+warnings.filterwarnings('ignore')
+
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    pc = pc / m
+    return pc
+
+
+def farthest_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:, :3]
+    centroids = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
+    for i in range(npoint):
+        centroids[i] = farthest
+        centroid = xyz[farthest, :]
+        dist = np.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = np.argmax(distance, -1)
+    point = point[centroids.astype(np.int32)]
+    return point
+
+
+class TranslationDataLoader(Dataset):
+    def __init__(self, root, npoint=1024, split='train', uniform=False, normal_channel=True, cache_size=15000):
+        self.root = root
+        self.npoints = npoint
+        self.uniform = uniform
+        self.split = split
+
+        self.normal_channel = normal_channel
+
+        shape_ids = {}
+        shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.root, 'pointdata_train.txt'))]
+        shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.root, 'pointdata_test.txt'))]
+
+        assert (split == 'train' or split == 'test')
+        # list of (shape_name, shape_txt_file_path) tuple
+        self.datapath = [(os.path.join(self.root, shape_ids[split][i])) for i in range(len(shape_ids[split]))]
+        print('The size of %s data is %d' % (split, len(self.datapath)))
+
+        self.cache_size = cache_size  # how many data points to cache in memory
+        self.cache = {}  # from index to (point_set, cls) tuple
+
+    def __len__(self):
+        return len(self.datapath)
+
+    def _get_item(self, index):
+        if index in self.cache:
+            point_set = self.cache[index]
+        else:
+            fn = self.datapath[index]  # file name
+            point_set = np.loadtxt(fn).astype(np.float32)
+            if len(self.cache) < self.cache_size:
+                self.cache[index] = point_set
+
+        point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+
+        if not self.normal_channel:
+            point_set = point_set[:, 0:3]
+
+        return point_set
+
+    def __getitem__(self, index):
+        return self._get_item(index)
+
+
+if __name__ == '__main__':
+    import torch
+
+    data = TranslationDataLoader('./data/point_data/', split='train', uniform=False, normal_channel=True)
+    DataLoader = torch.utils.data.DataLoader(data, batch_size=12, shuffle=True)
+    for point, label in DataLoader:
+        import ipdb;
+
+        ipdb.set_trace()
+        print(point.shape)
+        print(label.shape)
