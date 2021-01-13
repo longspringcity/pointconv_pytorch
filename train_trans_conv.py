b4902e2b9318726c8bf64bed58ac4befd32307ac
longspringcity <1277535055@qq.com>
2021-01-13 15:25:16 +0800

    small changes

--- train_trans_conv.py
+++ train_trans_conv.py
@@ -164,7 +164,6 @@ def main(args):
             distance = torch.norm(diff, dim=1)
             correct = torch.sum(distance < 0.2)
             mean_correct.append(correct.item() / float(points.size()[0]))
-            print(mean_correct)
             loss.backward()
             optimizer.step()
             global_step += 1
