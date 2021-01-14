21a9fb6797f2f5d078f22945608acf55fcecdab5
longspringcity <1277535055@qq.com>
2021-01-14 11:50:23 +0800

    small changes

--- data_utils/TranslationDataLoader.py
+++ data_utils/TranslationDataLoader.py
@@ -10,8 +10,8 @@ warnings.filterwarnings('ignore')
 def pc_normalize(pc):
     centroid = np.mean(pc, axis=0)
     pc = pc - centroid
-    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
-    pc = pc / m
+    # m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    # pc = pc / m
     return pc
 
 
