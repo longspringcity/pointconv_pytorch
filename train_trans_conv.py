1ee2d7f61112bc3054f18ee5cf911450ab5fc38d
longspringcity <1277535055@qq.com>
2021-01-17 12:06:28 +0800

    small changes

--- train_trans_conv.py
+++ train_trans_conv.py
@@ -20,7 +20,7 @@ from utils.utils import trans_test, save_checkpoint
 def parse_args():
     '''PARAMETERS'''
     parser = argparse.ArgumentParser('PointConv')
-    parser.add_argument('--batchsize', type=int, default=10, help='batch size in training')
+    parser.add_argument('--batchsize', type=int, default=5, help='batch size in training')
     # parser.add_argument('--batchsize', type=int, default=2, help='batch size in training')
     parser.add_argument('--epoch', default=100, type=int, help='number of epoch in training')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
